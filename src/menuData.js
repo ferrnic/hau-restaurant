4ec7bb4606d7a0566d4206003f89003c363df2c6
Camille <aziaziazi@gmail.com>
2017-11-11 16:26:27 +0100

    Add mock menu datas

---
+++ src/menuData.js
@@ -0,0 +1,42 @@
+const carte = [
+  {
+    name: "Potages",
+    items: [
+      { name: "Potage aux cheveux d'ange et au poulet", price: 7 },
+      { name: "Potage piquant au poivre oriental", price: 7 },
+      { name: "Potage aux raviolis de scampis", price: 8 },
+      { name: "Potage saïgonnais", price: 8 },
+      { name: "Potage du chef au santé", price: 8 },
+    ]
+  },
+  {
+    name: "Entrées froides",
+    items: [
+      { name: "Boeuf cru piquant", price: 13 },
+      { name: "Salade de cheveux d'ange", price: 10 },
+      { name: "Salade d'ananas et de scampis", price: 12 },
+    ]
+  },
+  {
+    name: "Entrées vapeurs",
+    items: [
+      { name: "Bouchées de scampis", price: 8 },
+      { name: "Bouchées de porc et de scampis", price: 8 },
+      { name: "Bouchées de roses", price: 8 },
+      { name: "Filet de poulet aux feuilles de bananier", price: 9 },
+      { name: "Crêpes tonkinoises", price: 9 },
+    ]
+  },
+  {
+    name: "Entrées chaudes",
+    items: [
+      { name: "Raviolis croustillants", price: 8 },
+      { name: "Croquettes vietnamiennes", price: 9 },
+      { name: "Brochettes de filet de poulet au saté", price: 8 },
+      { name: "Aile de poulet farcie", price: 7 },
+      { name: "Croustillons de scampis", price: 9 },
+      { name: "Scampis aux épices", price: 12 },
+      { name: "Assiette Hau (2 personnes)", price: 24 },
+    ]
+  },
+];
