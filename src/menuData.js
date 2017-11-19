bbac4365837937683eb4e90b371d32a581650c49
Camille <aziaziazi@gmail.com>
2017-11-19 21:34:31 +0100

    update menu data

--- src/menuData.js
+++ src/menuData.js
@@ -7,6 +7,7 @@ export const carte = [
       { name: "Potage aux raviolis de scampis", price: 8 },
       { name: "Potage saïgonnais", price: 8 },
       { name: "Potage du chef au santé", price: 8 },
+      { name: "Super loooooooooooooooooooooooooooog loooooooooooooooooooooooooooog loooooooooooooooooooooooooooog item with super long price", price: 999 },
     ]
   },
   {
@@ -46,6 +47,51 @@ export const vins = [
     name: "champ",
     items: [
       { name: "champomy", price: 1 },
+    ]
+  },{
+    name: "Chile",
+    items: [
+      { name: "Los Vascos Rose", price: 2015 },
+    ]
+  },{
+    name: "France",
+    items: [
+      { name: "Albert Boxler Edelzwicker Reserve", price: 2012 },
+      { name: "FAT bastard Shiraz", price: 2013},
+    ]
+  },{
+    name: "Spain",
+    items: [
+      { name: "Encanto Mencia Roble", price: 2011 },
+      { name: "Campo Viejo Garnacha", price: 2012 },
+      { name: "Love Over Money Basca", price: 2013 },
+    ]
+  },{
+    name: "UnitedStates",
+    items: [
+      { name: "Robert Mondavi Twin Oaks Shiraz", price: 2013 },
+      { name: "Bully Hill Vineyards Love My Goat", price: 2012 },
+      { name: "Menage a Trois Merlot", price: 2014 },
+      { name: "HRM Rex Goliath Chardonnay", price: 2015}
+    ],
+  },{
+    name: "Australia",
+    items: [
+      { name: "Yellowtail Sweet Red Roo", price: 2015  },
+      { name: "Yellowtail Shiraz Grenache", price: 2015 },
+      { name: "Yellowtail Cabernet Sauvignon", price: 2015 },
+      { name: "Turkey Flat Butcher's Block", price: 2012 },
+    ],
+  },{
+    name: "Italy",
+    items: [
+      { name: "Corte Giara Pinot Grigio", price: 2014  },
+      { name: "Elmo Pio Moscato", price: 2014 },
+      { name: "Roscato Rosse Dolce", price :2015 },
+      { name: "Marchesi Mazzei Zisola Doppiozeta Noto Rosso", price: 2008 },
+      { name: "Elmo Pio Piemonte Sweet Red Brachetto", price: 2014 },
+      { name: "Cavit Pinot Noir", price: 2014 },
+      { name: "Castello Banfi Rosso di Montalcino Poggio alle Mura", price: 2012 },
     ]
   },
 ];
