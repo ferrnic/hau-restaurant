5b573e95f27bb83a0becc92eab306fc993c9b423
Camille <aziaziazi@gmail.com>
2017-12-19 22:41:26 +0100

    Add pictures with Cloudinary

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ThemeProvider } from "styled-components";
 import {BrowserRouter as Router, Route, Switch, Redirect} from "react-router-dom";
+import { CloudinaryContext } from "cloudinary-react";
 
 import { theme } from "./theme";
 import Home from "./views/Home";
@@ -9,11 +10,13 @@ import PagesWrapper from "./views/PagesWrapper";
 const App = () => (
   <ThemeProvider theme={theme}>
     <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/:path(carte|vins|restaurant)" component={PagesWrapper} />        
-        <Redirect from="/" to="/" />
-      </Switch>
+      <CloudinaryContext cloudName="hau">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/:path(carte|vins|restaurant)" component={PagesWrapper} />
+          <Redirect from="/" to="/" />
+        </Switch>
+      </CloudinaryContext>
     </Router>
   </ThemeProvider>
 );
---
+++ src/components/Slideshow/SliderMD.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import styled from 'styled-components';
+import Slider from 'react-slick';
+import { Image } from "cloudinary-react";
+
+const StyledImage = styled(Image)`
+  height: 100vh;
+  width: 100%;
+`;
+
+const Wrapper = styled.div`
+  background-color: cadetblue;
+  height: 100%;
+  width: 100%;
+`;
+
+const SliderMD = () => {
+  const handleClick = () => {
+    console.log('clicked')
+  };
+  
+  let settings = {
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    speed: 5000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  return (
+    <Wrapper onClick={handleClick}>
+      <Slider {...settings}>
+        <StyledImage publicId="Capture_d_écran_2017-12-06_à_21.50.13_ftrcx0.png" />
+      </Slider>
+    </Wrapper>
+  );
+};
+
+export default SliderMD;
