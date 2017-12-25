6ec404c81991890f2a2a916e9dc5205044a8176a
Camille <aziaziazi@gmail.com>
2017-12-25 10:57:25 +0100

    Fetch cloudinary links by tag

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,67 @@
 import React from 'react';
 import { ThemeProvider } from "styled-components";
-import {BrowserRouter as Router, Route, Switch, Redirect} from "react-router-dom";
-import { CloudinaryContext } from "cloudinary-react";
+import {BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { CloudinaryContext } from 'cloudinary-react';
+
+import axios from 'axios';
 
 import { theme } from "./theme";
 import Home from "./views/Home";
 import PagesWrapper from "./views/PagesWrapper";
 
-const App = () => (
-  <ThemeProvider theme={theme}>
-    <Router>
-      <CloudinaryContext cloudName="hau">
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/:path(carte|vins|restaurant)" component={PagesWrapper} />
-          <Redirect from="/" to="/" />
-        </Switch>
-      </CloudinaryContext>
-    </Router>
-  </ThemeProvider>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      galleries: {
+        restaurant: [],
+        carte: [],
+        vin: [],
+      }
+    }
+  }
+
+  fetchList(tag) {
+    axios.get(`https://res.cloudinary.com/hau/image/list/${tag}.json`)
+      .then(res => {
+        this.setState({
+          galleries: {
+            ...this.state.galleries,
+            [tag]: res.data.resources,
+          }
+        });
+      });
+  }
+
+  componentDidMount() {
+    this.fetchList('restaurant');
+    this.fetchList('vins');
+    this.fetchList('carte');
+  }
+
+  render() {
+    return (
+      <ThemeProvider theme={theme}>
+        <Router>
+          <CloudinaryContext
+            cloudName="hau"
+            width="auto"
+          >
+            <Switch>
+              <Route
+                path="/:path(carte|vins|restaurant)"
+                component={PagesWrapper}
+              />
+              <Route
+                path="/"
+                component={Home}
+              />
+            </Switch>
+          </CloudinaryContext>
+        </Router>
+      </ThemeProvider>
+    );
+  };
+}
 
 export default App;
