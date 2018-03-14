cbc07a03c51aaf335030d382b152b4cb980a0248
camille <aziaziazi@gmail.com>
2018-03-15 00:06:00 +0100

    update homevideo publicID

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -78,7 +78,7 @@ const Home = () => (
   <BackGround>
     <StyledVideo
       cloudName="hau"
-      publicId="march"
+      publicId="current-march"
       autoPlay
       loop
       poster={null}
