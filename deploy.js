41d846dfd6dcb55d657ce59f0bb926d80b919dd8
c.gabrieli <c.gabrieli@meetic-corp.com>
2019-01-19 06:23:38 +0100

    mend

--- deploy.js
+++ deploy.js
@@ -1,26 +1,21 @@
 const FtpDeploy = require('ftp-deploy')
 const ftpDeploy = new FtpDeploy()
 
-const localRoot = './build'
-const remoteRoot = '/www/'
 const config = {
   host: process.env.HOST,
   password: process.env.PASSWORD,
   user: process.env.USER,
-  localRoot: localRoot,
-  remoteRoot: remoteRoot,
+  localRoot: './build',
+  remoteRoot: '/www/',
   include: [ '*', '**/*' ],
   exclude: [ 'dist/**/*.map' ],
 }
 console.log('--------- will deploy with config => ', config);
 ftpDeploy.deploy(config, (err, res) => {
-  console.log('hasDeployed');
   if (err) {
-    console.log('*********error');
     console.log(err)
   }
   else {
-    console.log(`**********filed moved from (localRoot) ${localRoot} to (remoteRoot) ${remoteRoot} :`);
     console.log(res);
   }
 });
--- src/components/HomeSeed/Seed.jsx
+++ src/components/HomeSeed/Seed.jsx
@@ -30,6 +30,7 @@ const Seed = (props) => {
     <StyledSeed
       viewBox="0 0 120 120"
       xmlns="http://www.w3.org/2000/svg"
+      xmlSpace="preserve"
       show={show}
       className={className}
       {...rest}
