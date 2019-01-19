f300542bc20df0d12e036d8542ffe36f82c60a98
c.gabrieli <c.gabrieli@meetic-corp.com>
2019-01-19 06:05:07 +0100

    mend

--- deploy.js
+++ deploy.js
@@ -1,36 +1,5 @@
 const FtpDeploy = require('ftp-deploy')
 const ftpDeploy = new FtpDeploy()
-var fs = require('fs');
-
-var path1 = __dirname;
-var path2 = './';
-var path3 = '';
-
-fs.readdir(path1, function(err, items) {
-  console.log('....path1 => ', path1);
-  console.log(items);
-
-  for (var i=0; i<items.length; i++) {
-    console.log(items[i]);
-  }
-});
-fs.readdir(path2, function(err, items) {
-  console.log('....path2 => ', path2);
-  console.log(items);
-
-  for (var i=0; i<items.length; i++) {
-    console.log(items[i]);
-  }
-});
-fs.readdir(path3, function(err, items) {
-  console.log('....path3 => ', path3);
-  console.log(items);
-
-  for (var i=0; i<items.length; i++) {
-    console.log(items[i]);
-  }
-});
-
 
 const localRoot = __dirname + '/build'
 const remoteRoot = '/test/'
