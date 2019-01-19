bfcecde61ba082dc770f10061838c2e06a19f761
c.gabrieli <c.gabrieli@meetic-corp.com>
2019-01-19 05:38:10 +0100

    mend

--- deploy.js
+++ deploy.js
@@ -15,7 +15,10 @@ const config = {
 }
 console.log('--------- will deploy with config => ', config);
 ftpDeploy.deploy(config, (err, res) => {
-  if (err) console.log(err)
+  if (err) {
+    console.log('error');
+    console.log(err)
+  }
   else {
     console.log(`filed moved from (localRoot) ${localRoot} to (remoteRoot) ${remoteRoot} :`);
     console.log(res);
