9a0658315d779c899cb053ee4a34094966fd3543
c.gabrieli <c.gabrieli@meetic-corp.com>
2019-01-19 04:34:33 +0100

    test if deployment is effective

--- src/index.js
+++ src/index.js
@@ -5,5 +5,7 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
+console.log("welcome in Hau's console");
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
