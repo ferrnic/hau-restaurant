2a2cd1d176e202d7441efc773ec2fe556d3e1636
c.gabrieli <c.gabrieli@meetic-corp.com>
2019-01-19 04:56:05 +0100

    todo: supprimer ces commmits innutiles

--- src/index.js
+++ src/index.js
@@ -5,7 +5,5 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-console.log("welcome in Hau's console");
-
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
