24d282d1993219571d8e83609c4b1e4b4338e8e7
Camille Gabrieli <c.gabrieli@meetic-corp.com>
2020-06-08 12:09:02 +0200

    fix typo

--- src/components/Carte/RestaurantCarte.jsx
+++ src/components/Carte/RestaurantCarte.jsx
@@ -34,7 +34,7 @@ const RestaurantCarte = (props) => {
             <ItemLine>
               <ItemName>
                 <Tough>Bienvenue chez Hau.</Tough> <br />
-                <Tough>Vietnamien</Tough> d’origine, chez <Tough>Hau</Tough> on y mange sa <Tough>cuisine</Tough>. Dans ce cadre unique rempli de <Tough>saveurs et d’histoire</Tough>, le chef vous propose une convivialité bien à lui : une <Tough>cuisine fine aux saveurs asiatiques, fraîche</Tough>, et toujours repensée, accompagnée d’une belle carte de <Tough>vin biologique et naturelle</Tough>. Un voyage <Tough>gasonomique</Tough> proposé à <Tough>midi</Tough> comme au <Tough>soir</Tough>. <br />
+                <Tough>Vietnamien</Tough> d’origine, chez <Tough>Hau</Tough> on y mange sa <Tough>cuisine</Tough>. Dans ce cadre unique rempli de <Tough>saveurs et d’histoire</Tough>, le chef vous propose une convivialité bien à lui : une <Tough>cuisine fine aux saveurs asiatiques, fraîche</Tough>, et toujours repensée, accompagnée d’une belle carte de <Tough>vin biologique et naturelle</Tough>. Un voyage <Tough>gastronomique</Tough> proposé à <Tough>midi</Tough> comme au <Tough>soir</Tough>. <br />
                 <Link href={process.env.PUBLIC_URL + "/chronique_pierre_lecomte.pdf"} download>Lisez la chronique de Pierre Lecomte sur le restaurant de Hau</Link>.
               </ItemName>
             </ItemLine>
